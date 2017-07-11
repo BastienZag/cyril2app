47673135e8d884576da8e618553681f8b69c652f
Bastien <b.cabirou@gmail.com>
2017-07-11 16:30:16 +1000

    ugly fix for Cyril

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule2 } from '../app2/app.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,3 +16,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule2);
